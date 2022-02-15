30dfc1eb581f34798557959ac1951077f5994aa6
F8Loso <kf@techworld.support>
2022-02-15 13:09:38 -0600

    1.) Compressed files from MyWorkspace, *important content a. these files will store intel
    2.) In case any files are misplaced contact -- kf@techworld.support

---
+++ JavaWS/Day5/src/com/revature/MyClass.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("  $$ I'm on my way ** ");
+	}
+
+}
---
+++ JavaWS/Day6/src/com/MyRevAdven/MyWork.java
@@ -0,0 +1,13 @@
+package com.MyRevAdven;
+import java.util.Date;
+
+public class MyWork {
+
+	public static void main(String[] args) {
+		
+	java.sql.Date current = new java.sql.Date(122,1,14);
+		
+		System.out.println(current);
+		
+	}
+}
